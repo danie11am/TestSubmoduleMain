37c926e63ab9515e1b5c2bb47dca458de8fbb42a
Daniel Lam <im@daniellam.name>
2014-09-24 09:37:20 +1000

    Test change.

--- TestSubmoduleMain/ViewController.m
+++ TestSubmoduleMain/ViewController.m
@@ -15,6 +15,8 @@
 
 @implementation ViewController
 
+// Original master branch change - for the sake of it.
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
